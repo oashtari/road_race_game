2c5b1b3ae884bb8cb8663a8f04d2d6a83d151e59
omidashtari <oashtari@gmail.com>
2023-08-10 08:18:24 -0700

    added obstacles, health score, collisions leading to score reduction, ending game at 0, with game over sound effects, and stopping the game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use rusty_engine::prelude::*;
-
+use rand::prelude::*;
+use rusty_engine::{
+    game,
+    prelude::{bevy::ecs::event, *},
+};
 struct GameState {
     health_amount: u8,
     lost: bool,
@@ -10,6 +13,9 @@ const ROAD_SPEED: f32 = 400.0;
 
 fn main() {
     let mut game = Game::new();
+
+    let health_message = game.add_text("health_message", "Health: 5");
+    health_message.translation = Vec2::new(550.0, 320.0);
 
     // game setup goes here
 
@@ -22,6 +28,20 @@ fn main() {
         let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierRed);
         roadline.scale = 0.1;
         roadline.translation.x = -600.0 + 150.0 * i as f32;
+    }
+
+    let obstacle_presets = vec![
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingConeStraight,
+    ];
+
+    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+        let obstacle = game.add_sprite(format!("obstacle{}", i), preset);
+        obstacle.layer = 5.0;
+        obstacle.collision = true;
+        obstacle.translation.x = thread_rng().gen_range(800.0..1600.0);
+        obstacle.translation.y = thread_rng().gen_range(-300.0..300.0);
     }
 
     game.audio_manager
@@ -38,6 +58,10 @@ fn main() {
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     // game logic goes here
     let mut direction = 0.0;
+
+    if game_state.lost {
+        return;
+    }
 
     if engine.keyboard_state.pressed(KeyCode::Up) {
         direction += 1.0;
@@ -60,5 +84,33 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                 sprite.translation.x += 1500.0;
             }
         }
+
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
+            }
+        }
+    }
+
+    let health_message = engine.texts.get_mut("health_message").unwrap();
+    for event in engine.collision_events.drain(..) {
+        if !event.pair.either_contains("player1") || event.state.is_end() {
+            continue;
+        };
+        if game_state.health_amount > 0 {
+            game_state.health_amount -= 1;
+            health_message.value = format!("Health: {}", game_state.health_amount);
+            engine.audio_manager.play_sfx(SfxPreset::Impact3, 0.5);
+        }
+    }
+
+    if game_state.health_amount == 0 {
+        game_state.lost = true;
+        let game_over = engine.add_text("game over", "Game Over");
+        game_over.font_size = 128.0;
+        engine.audio_manager.stop_music();
+        engine.audio_manager.play_sfx(SfxPreset::Jingle3, 0.5);
     }
 }
