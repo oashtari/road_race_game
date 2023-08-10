80b9ea5e24e5fa6a72b14f1ebf206071e27eb6b1
omidashtari <oashtari@gmail.com>
2023-08-10 07:52:54 -0700

    added road lines, ability to move car

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ struct GameState {
     lost: bool,
 }
 
+const PLAYER_SPEED: f32 = 250.0;
+const ROAD_SPEED: f32 = 400.0;
+
 fn main() {
     let mut game = Game::new();
 
@@ -15,8 +18,16 @@ fn main() {
     player1.layer = 10.0;
     player1.collision = true;
 
+    for i in 0..10 {
+        let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierRed);
+        roadline.scale = 0.1;
+        roadline.translation.x = -600.0 + 150.0 * i as f32;
+    }
+
     game.audio_manager
         .play_music(MusicPreset::WhimsicalPopsicle, 0.2);
+
+    game.add_logic(game_logic);
 
     game.run(GameState {
         health_amount: 5,
@@ -26,4 +37,28 @@ fn main() {
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     // game logic goes here
+    let mut direction = 0.0;
+
+    if engine.keyboard_state.pressed(KeyCode::Up) {
+        direction += 1.0;
+    }
+    if engine.keyboard_state.pressed(KeyCode::Down) {
+        direction -= 1.0;
+    }
+
+    let player1 = engine.sprites.get_mut("player1").unwrap();
+    player1.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
+    player1.rotation = direction * 0.15;
+    if player1.translation.y < -360.0 || player1.translation.y > 360.0 {
+        game_state.health_amount = 0;
+    }
+    // Move road objects
+    for sprite in engine.sprites.values_mut() {
+        if sprite.label.starts_with("roadline") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -675.0 {
+                sprite.translation.x += 1500.0;
+            }
+        }
+    }
 }
